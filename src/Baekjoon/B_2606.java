6943f197698c91a00404c59c08f2799f49e54afb
pear_c <zxcv9676@naver.com>
2024-12-11 17:33:10 +0900

    [Baekjoon] 2606

--- src/Baekjoon/B_2606.java
+++ src/Baekjoon/B_2606.java
@@ -6,30 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class B_2606 {
+    static int N, M;
     static boolean[] visited;
     static List[] computers;
     static int count = 0;
-    static int N;
-    static int M;
 
-    static void DFS(int start) {
-        if(!visited[start]) {
-            visited[start] = true;
-            count++;
-            for(int i=0; i<computers[start].size(); i++) {
-                DFS((int)computers[start].get(i));
-            }
-        }
-    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // N, M 입력
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
+        // 컴퓨터 초기화
         visited = new boolean[N+1];
         computers = new List[N+1];
         for(int i=1; i<=N; i++) {
             computers[i] = new ArrayList<>();
         }
+        // 컴퓨터 정보 입력
         for(int i=1; i<=M; i++) {
             String[] s = br.readLine().split(" ");
             int a = Integer.parseInt(s[0]);
@@ -41,5 +34,15 @@ public class B_2606 {
         DFS(1);
 
         System.out.println(count-1);
+    }
+    static void DFS(int start) {
+        // 방문 하지 않은 노드 탐색
+        if(!visited[start]) {
+            visited[start] = true;
+            count++;
+            for(int i=0; i<computers[start].size(); i++) {
+                DFS((int)computers[start].get(i));
+            }
+        }
     }
 }

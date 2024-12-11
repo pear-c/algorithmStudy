0450a62bcb45baedd359e0c11b2cdeb6caf81dbe
pear_c <zxcv9676@naver.com>
2024-12-11 18:22:45 +0900

    [Baekjoon] 20436

--- src/Baekjoon/B_20436.java
+++ src/Baekjoon/B_20436.java
@@ -3,7 +3,6 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 

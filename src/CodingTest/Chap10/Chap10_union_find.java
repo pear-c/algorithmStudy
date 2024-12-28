b968e7cb76e2728306bdd1b5a606a25201edbacf
pear_c <zxcv9676@naver.com>
2024-12-28 22:29:34 +0900

    Chap10 - 서로소 집합(응용 : 사이클 발생확인)

--- src/CodingTest/Chap10/Chap10_union_find.java
+++ src/CodingTest/Chap10/Chap10_union_find.java
@@ -34,25 +34,44 @@ public class Chap10_union_find {
         for(int i=1; i<=v; i++)
             parent[i] = i;
 
-        // union 연산 수행
+//        // union 연산 수행
+//        for(int i=0; i<e; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            union_parent(parent, a, b);
+//        }
+//
+//        // 각 원소 집합 출력
+//        System.out.print("각 원소가 속한 집합: ");
+//        for(int i=1; i<=v; i++) {
+//            System.out.print(find_parent(parent, i) + " ");
+//        }
+//        System.out.println();
+//
+//        // 부모 테이블 출력
+//        System.out.print("부모 테이블: ");
+//        for(int i=1; i<=v; i++) {
+//            System.out.print(parent[i] + " ");
+//        }
+        // 응용 - 사이클 판별
+        boolean cycle = false;
+
         for(int i=0; i<e; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            union_parent(parent, a, b);
+            if(find_parent(parent, a) == find_parent(parent, b)) {
+                cycle = true;
+                break;
+            }
+            else {
+                union_parent(parent, a, b);
+            }
         }
-
-        // 각 원소 집합 출력
-        System.out.print("각 원소가 속한 집합: ");
-        for(int i=1; i<=v; i++) {
-            System.out.print(find_parent(parent, i) + " ");
-        }
-        System.out.println();
-
-        // 부모 테이블 출력
-        System.out.print("부모 테이블: ");
-        for(int i=1; i<=v; i++) {
-            System.out.print(parent[i] + " ");
-        }
+        if(cycle)
+            System.out.println("사이클 발생");
+        else
+            System.out.println("사이클 발생 X");
     }
 }

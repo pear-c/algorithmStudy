63820a30f6e8e5640cb2404b33d8ade2d352d324
pear_c <zxcv9676@naver.com>
2025-01-10 17:13:38 +0900

    이것이 코딩테스트다 - Greedy_02

--- src/CodingTest/Greedy/Greedy_02.java
+++ src/CodingTest/Greedy/Greedy_02.java
@@ -1,7 +1,21 @@
 package CodingTest.Greedy;
 
-public class Greedy_02 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Greedy_02 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        int result = input.charAt(0) - '0';
+        for(int i=0; i<input.length()-1; i++){
+            int n = input.charAt(i) - '0';
+            if(n == 0)
+                result += input.charAt(i+1) - '0';
+            else
+                result *= input.charAt(i+1) - '0';
+        }
+        System.out.println(result);
     }
 }

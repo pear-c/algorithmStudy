cdf84f0ed379c70c8048ef38ef691c245a70f6e3
pear_c <zxcv9676@naver.com>
2025-01-11 16:36:26 +0900

    이것이 코딩테스트다 - Greedy_03

--- src/CodingTest/Greedy/Greedy_03.java
+++ src/CodingTest/Greedy/Greedy_03.java
@@ -1,7 +1,26 @@
 package CodingTest.Greedy;
 
-public class Greedy_03 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Greedy_03 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        int zeroCount = 0;
+        int oneCount = 0;
+
+        int flag = input.charAt(0) - '0';
+        if(flag == 0) zeroCount++;
+        else oneCount++;
+
+        for(int i = 1; i < input.length(); i++){
+            if(input.charAt(i) != flag){
+                
+            }
+        }
+
+        System.out.println(Math.min(zeroCount, oneCount));
     }
 }

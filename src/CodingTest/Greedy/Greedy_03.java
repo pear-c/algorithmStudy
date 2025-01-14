9866bc3805e60589aa8274383c09b80baa1bd168
pear_c <zxcv9676@naver.com>
2025-01-14 18:10:38 +0900

    이것이 코딩테스트다 - Greedy_03

--- src/CodingTest/Greedy/Greedy_03.java
+++ src/CodingTest/Greedy/Greedy_03.java
@@ -5,23 +5,29 @@ import java.io.InputStreamReader;
 
 public class Greedy_03 {
     public static void main(String[] args) throws Exception {
+        // 1. 전체 문자열 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
 
+        // 2. 0, 1 이 연속되는 부분 나눔
         int zeroCount = 0;
         int oneCount = 0;
 
-        int flag = input.charAt(0) - '0';
-        if(flag == 0) zeroCount++;
-        else oneCount++;
+        if(input.charAt(0) == '0')
+            zeroCount++;
+        else
+            oneCount++;
 
-        for(int i = 1; i < input.length(); i++){
-            if(input.charAt(i) != flag){
-                oneCount++;
-
+        for(int i=0; i<input.length()-1; i++) {
+            if (input.charAt(i) != input.charAt(i + 1)) {
+                if (input.charAt(i + 1) == '1')
+                    zeroCount++;
+                else
+                    oneCount++;
             }
         }
 
+        // 3. 두 부분 중 더 작은 부분의 개수 출력
         System.out.println(Math.min(zeroCount, oneCount));
     }
 }

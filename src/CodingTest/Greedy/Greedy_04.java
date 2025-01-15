22951c23395a76ba323cc3f44c1ba8c11596a4ad
pear_c <zxcv9676@naver.com>
2025-01-15 18:02:30 +0900

    이것이 코딩테스트다 - Greedy_04

--- src/CodingTest/Greedy/Greedy_04.java
+++ src/CodingTest/Greedy/Greedy_04.java
@@ -10,7 +10,6 @@ public class Greedy_04 {
     public static int[] arr;
 
     public static void main(String[] args) throws IOException {
-        // 1. N 입력, 각 동전 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
@@ -19,7 +18,6 @@ public class Greedy_04 {
             arr[i] = Integer.parseInt(input[i]);
         }
 
-        // 2. 각 동전으로 만들 수 있는 경우의 수 모두 계산
         Arrays.sort(arr);
 
         int target = 1;

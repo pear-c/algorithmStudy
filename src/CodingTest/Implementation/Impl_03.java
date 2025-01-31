e54e79dd0a995f1f5d3946538df36fda6b6b578a
pear_c <zxcv9676@naver.com>
2025-01-31 17:57:56 +0900

    이것이 코딩테스트다 - 구현_08)

--- src/CodingTest/Implementation/Impl_03.java
+++ src/CodingTest/Implementation/Impl_03.java
@@ -1,7 +1,12 @@
 package CodingTest.Implementation;
 
 public class Impl_03 {
+    class Student {
+        private String name;
+        private int age;
+    }
     public static void main(String[] args) {
         System.out.println();
     }
+
 }

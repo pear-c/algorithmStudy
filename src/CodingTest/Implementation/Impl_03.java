d1a4bb76fcfa68f6152d97353b4dab6dee130399
pear_c <zxcv9676@naver.com>
2025-02-15 16:05:10 +0900

    이것이 코딩테스트다 - 구현_09) 문자열 압축

--- src/CodingTest/Implementation/Impl_03.java
+++ src/CodingTest/Implementation/Impl_03.java
@@ -1,13 +1,43 @@
 package CodingTest.Implementation;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+// [연습문제 - 구현] Q.09 - 문자열 압축
 public class Impl_03 {
-    class Student {
-        private String name;
-        private int age;
+    static int solution(String s) {
+        int len = s.length();
+        int min = len;
+
+        for(int i=1; i<=len / 2; i++) {
+            StringBuilder sb = new StringBuilder();
+            String prevText = s.substring(0, i);
+            int count = 1;
+
+            for(int j=i; j<=len; j+=i) {
+                String splitText = s.substring(j, Math.min(j+i, len));
+                if(splitText.equals(prevText)) {
+                    count++;
+                } else {
+                    if(count > 1) {
+                        sb.append(count);
+                    }
+                    count = 1;
+                    sb.append(prevText);
+                    prevText = splitText;
+                }
+            }
+            sb.append(prevText);
+            min = Math.min(min, sb.length());
+        }
+        return min;
     }
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
+        String s = "aabbaccc";
+        System.out.println(solution(s));
+
     }
 
 }

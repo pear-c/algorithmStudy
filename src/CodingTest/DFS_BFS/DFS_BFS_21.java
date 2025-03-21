01f97a3bb87e4fe5fd5e5a1c00baee688f9ed963
pear_c <zxcv9676@naver.com>
2025-03-21 17:48:48 +0900

    [백준 16234] 인구 이동

--- src/CodingTest/DFS_BFS/DFS_BFS_21.java
+++ src/CodingTest/DFS_BFS/DFS_BFS_21.java
@@ -1,17 +1,13 @@
 package CodingTest.DFS_BFS;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 // DFS/BFS Q.21 - 인구 이동
 public class DFS_BFS_21 {
     static int N, L, R;
     static int[][] map;
     static boolean[][] visited;
-    static List<int[]> crews = new ArrayList<>(); // 연합
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
     static int count = 0;
@@ -31,38 +27,75 @@ public class DFS_BFS_21 {
         }
 
         int[][] prev;
-        do {
+        while(true) {
             prev = new int[N][N];
             for(int j=0; j<N; j++) {
                 prev[j] = map[j].clone();
             }
 
-            for(int j=0; j<N; j++) {
-                for(int i=0; i<N; i++) {
-                    makeCrew(j, i);
-                }
-            }
+            reBfs();
 
-            if(!crews.isEmpty()) {
-                movePerson();
-                count++;
+            if(isEqual(prev, map)) {
+                break;
             }
-        } while(!isEqual(prev, map));
+            count++;
+        }
 
         System.out.println(count);
     }
 
-    static void movePerson() {
-        int sum = 0;
-        for(int i=0; i<crews.size(); i++) {
-            sum += map[crews.get(i)[0]][crews.get(i)[1]];
+    static void reBfs() {
+        A: for(int j=0; j<N; j++) {
+            for(int i=0; i<N; i++) {
+                if(bfs(j, i))
+                    break A;
+                visited = new boolean[N][N];
+            }
         }
-        int avg = sum / crews.size();
+    }
 
-        for(int[] pos : crews) {
-            map[pos[1]][pos[0]] = avg;
+
+    static boolean bfs(int y, int x) {
+        Queue<int[]> queue = new LinkedList<>();
+        List<int[]> crewList = new ArrayList<>();
+        int sum = 0;
+
+        queue.add(new int[]{y, x});
+        visited[y][x] = true;
+
+        while(!queue.isEmpty()) {
+            int[] pos = queue.poll();
+            crewList.add(pos);
+
+            sum += map[pos[0]][pos[1]];
+
+            for(int d=0; d<4; d++) {
+                int ny = dy[d] + pos[0];
+                int nx = dx[d] + pos[1];
+
+                if(nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                    int sub = Math.abs(map[ny][nx] - map[pos[0]][pos[1]]);
+
+                    if(sub >= L && sub <= R) {
+                        if (!visited[ny][nx]) {
+                            queue.add(new int[]{ny, nx});
+                            visited[ny][nx] = true;
+                        }
+                    }
+                }
+            }
         }
-        crews.clear();
+
+        if(crewList.size() <= 1) {
+            visited[y][x] = false;
+            return false;
+        }
+
+        int avg = sum / crewList.size();
+        for(int[] pos : crewList) {
+            map[pos[0]][pos[1]] = avg;
+        }
+        return true;
     }
 
     static boolean isEqual(int[][] prev, int[][] map) {
@@ -73,29 +106,5 @@ public class DFS_BFS_21 {
             }
         }
         return true;
-    }
-
-
-    static void makeCrew(int y, int x) {
-        for(int i=0; i<4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-
-            if(ny >= 0 && nx >= 0 && ny < N && nx < N) {
-                if(!visited[ny][nx]) {
-                    int sub = Math.abs(map[y][x] - map[ny][nx]);
-
-
-                    if(sub >= L && sub <= R) {
-                        if(!visited[y][x]) {
-                            crews.add(new int[]{y, x});
-                            visited[y][x] = true;
-                        }
-                        crews.add(new int[]{ny, nx});
-                        visited[ny][nx] = true;
-                    }
-                }
-            }
-        }
     }
 }

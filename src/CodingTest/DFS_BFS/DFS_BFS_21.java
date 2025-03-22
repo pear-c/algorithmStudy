6097adefc6134cacc209e9e7aa122197d6c2e7e7
pear_c <zxcv9676@naver.com>
2025-03-22 12:31:33 +0900

    [백준 16234] 인구 이동

--- src/CodingTest/DFS_BFS/DFS_BFS_21.java
+++ src/CodingTest/DFS_BFS/DFS_BFS_21.java
@@ -14,97 +14,69 @@ public class DFS_BFS_21 {
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-
         N = sc.nextInt();
         L = sc.nextInt();
         R = sc.nextInt();
         map = new int[N][N];
-        visited = new boolean[N][N];
         for(int j=0; j<N; j++) {
             for(int i=0; i<N; i++) {
                 map[j][i] = sc.nextInt();
             }
         }
 
-        int[][] prev;
         while(true) {
-            prev = new int[N][N];
+            visited = new boolean[N][N];
+            boolean moved = false;
+
             for(int j=0; j<N; j++) {
-                prev[j] = map[j].clone();
+                for(int i=0; i<N; i++) {
+                    if(!visited[j][i]) {
+                        if(bfs(j, i)) moved = true;
+                    }
+                }
             }
 
-            reBfs();
-
-            if(isEqual(prev, map)) {
-                break;
-            }
+            if(!moved) break;
             count++;
         }
-
         System.out.println(count);
     }
-
-    static void reBfs() {
-        A: for(int j=0; j<N; j++) {
-            for(int i=0; i<N; i++) {
-                if(bfs(j, i))
-                    break A;
-                visited = new boolean[N][N];
-            }
-        }
-    }
-
 
     static boolean bfs(int y, int x) {
         Queue<int[]> queue = new LinkedList<>();
         List<int[]> crewList = new ArrayList<>();
-        int sum = 0;
-
         queue.add(new int[]{y, x});
+        crewList.add(new int[]{y, x});
         visited[y][x] = true;
+        int sum = map[y][x];
 
         while(!queue.isEmpty()) {
             int[] pos = queue.poll();
-            crewList.add(pos);
-
-            sum += map[pos[0]][pos[1]];
 
             for(int d=0; d<4; d++) {
                 int ny = dy[d] + pos[0];
                 int nx = dx[d] + pos[1];
 
-                if(nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                if(nx >= 0 && ny >= 0 && nx < N && ny < N && !visited[ny][nx]) {
                     int sub = Math.abs(map[ny][nx] - map[pos[0]][pos[1]]);
 
                     if(sub >= L && sub <= R) {
-                        if (!visited[ny][nx]) {
-                            queue.add(new int[]{ny, nx});
-                            visited[ny][nx] = true;
-                        }
+                        queue.add(new int[]{ny, nx});
+                        crewList.add(new int[]{ny, nx});
+                        visited[ny][nx] = true;
+                        sum += map[ny][nx];
                     }
                 }
             }
         }
 
-        if(crewList.size() <= 1) {
-            visited[y][x] = false;
-            return false;
-        }
-
-        int avg = sum / crewList.size();
-        for(int[] pos : crewList) {
-            map[pos[0]][pos[1]] = avg;
-        }
-        return true;
-    }
-
-    static boolean isEqual(int[][] prev, int[][] map) {
-        for(int j=0; j<N; j++) {
-            for(int i=0; i<N; i++) {
-                if(map[j][i] != prev[j][i])
-                    return false;
+        if(crewList.size() > 1) {
+            int avg = sum / crewList.size();
+            for(int[] pos : crewList) {
+                map[pos[0]][pos[1]] = avg;
             }
+            return true;
         }
-        return true;
+        return false;
     }
 }

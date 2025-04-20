d65b5f761e38e624cd726a9d66779755f473ab12
pear_c <zxcv9676@naver.com>
2025-04-20 17:04:26 +0900

    [백준 2609] GCD, LCM

--- src/Baekjoon/Main.java
+++ src/Baekjoon/Main.java
@@ -5,32 +5,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
 
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<N; i++) {
-            list.add(sc.nextInt());
+        String input = sc.nextLine();
+        input = input.replace(" ", "");
+
+        if(input.equals("12345678")) {
+            System.out.println("ascending");
+        } else if(input.equals("87654321")) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
         }
-
-        Collections.sort(list);
-
-
-        int prev = 0;
-        A: for(int i=0; i<N-2; i++) {
-            for(int j=i+1; j<N-1; j++) {
-                for(int k=j+1; k<N; k++) {
-                    int sum = list.get(i) + list.get(j) + list.get(k);
-                    if(sum == M) {
-                        System.out.println(sum);
-                        break A;
-                    } else if(sum > M) {
-                        System.out.println(prev);
-                        break A;
-                    }
-                    prev = sum;
-                }
-            }
-         }
     }
 }
